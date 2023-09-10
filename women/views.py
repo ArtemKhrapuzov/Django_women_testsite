1262bf022a3c545713ddbf0e3968e2bc8ad0563e
Artem Khrapuzov <tema102@mail.ru>
2023-09-10 16:36:22 +0600

    create html/css

--- women/views.py
+++ women/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
+from .models import Women
 
 def index(request):
-    return HttpResponse("Страница приложения Women")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts})
+
+def categories(request, catid):
+    context = {
+        'catid': catid
+    }
+    return render(request, 'women/about.html', context=context)
