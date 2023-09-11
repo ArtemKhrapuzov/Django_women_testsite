fe6b265cd087103dfda8d8485b5ad930a04fb790
Artem Khrapuzov <tema102@mail.ru>
2023-09-11 12:47:42 +0600

    create Model Category, and foreignKey connection

--- women/urls.py
+++ women/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('cats/<int:catid>/', categories),
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- women/views.py
+++ women/views.py
@@ -1,14 +1,52 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
-from .models import Women
+from .models import Women, Category
+
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'},
+]
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts})
-
-def categories(request, catid):
+    cats = Category.objects.all()
     context = {
-        'catid': catid
+        'posts': posts,
+        'menu': menu,
+        'cats': cats,
+        'cat_selected': 0,
     }
-    return render(request, 'women/about.html', context=context)
+    return render(request, 'women/index.html', context=context)
+
+def about(request):
+    return render(request, 'women/about.html')
+
+
+def addpage(request):
+    return HttpResponse('добавить')
+
+def contact(request):
+    return HttpResponse('контакты')
+
+def login(request):
+    return HttpResponse('логин')
+
+def show_post(request, post_id):
+    post = Women.objects.get(id=post_id)
+    return HttpResponse(f'post {post_id}')
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'cats': cats,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
