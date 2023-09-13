5a21ef4926e064b41b172c895494e9b5e9c5360f
Artem Khrapuzov <tema102@mail.ru>
2023-09-13 21:15:03 +0600

    create register

--- women/views.py
+++ women/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Women, Category
-from .forms import AddPostForm
+from .forms import AddPostForm, RegisterUserForm
 from .utils import *
 
 
@@ -72,3 +72,19 @@ class WomenCategory(DataMixin, ListView):
 
     def get_queryset(self):
         return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+
+
+
