587abad962e14808f72b2006914c96f3fce53ca4
Artem Khrapuzov <tema102@mail.ru>
2023-09-14 13:16:32 +0600

    create authenticated

--- women/views.py
+++ women/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth import logout, login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Women, Category
-from .forms import AddPostForm, RegisterUserForm
+from .forms import AddPostForm, RegisterUserForm, LoginUserForm
 from .utils import *
 
 
@@ -42,10 +44,6 @@ def contact(request):
     return HttpResponse('контакты')
 
 
-def login(request):
-    return HttpResponse('логин')
-
-
 class ShowPost(DataMixin, DetailView):
     model = Women
     template_name = 'women/post.html'
@@ -75,6 +73,7 @@ class WomenCategory(DataMixin, ListView):
 
 
 class RegisterUser(DataMixin, CreateView):
+    """Форма регистрации"""
     form_class = RegisterUserForm
     template_name = 'women/register.html'
     success_url = reverse_lazy('login')
@@ -84,7 +83,27 @@ class RegisterUser(DataMixin, CreateView):
         c_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(c_def.items()))
 
+    def form_valid(self, form):
+        """При успешной регистрации, переводит на главную страницу"""
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
 
 
+class LoginUser(DataMixin, LoginView):
+    """Форма авторизации"""
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
 
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    """Выход из авторизации"""
+    logout(request)
+    return redirect('login')
